221430f465c47c31283ac27a284344e04d049b4d
Michelle Heintze <michelle-heintze@web.de>
2021-10-05 21:18:45 +0200

    add users

--- js/addTask.js
+++ js/addTask.js
@@ -2,11 +2,33 @@
 let allTasks = [];
 setURL('http://gruppe-107.developerakademie.net/smallest_backend_ever');
 
+users = [{
+        'id': 0,
+        'image': 'GottfriedOswald.jpg',
+        'name': 'Gottfried O.'
+    },
+    {
+        'id': 1,
+        'image': 'michelleH.jpg',
+        'name': 'Michelle H.'
+    },
+    {
+        'id': 2,
+        'image': 'guest.png',
+        'name': 'Kaan'
+    },
+    {
+        'id': 3,
+        'image': 'guest.png',
+        'name': 'Guest'
+    },
+];
 
 async function init() {
     await downloadFromServer();
     allTasks = JSON.parse(backend.getItem('allTasks')) || [];
     console.log('Loaded from backend allTasks', allTasks);
+    showUsers();
 }
 
 function addTask() {
@@ -16,7 +38,11 @@ function addTask() {
     let date = document.getElementById('date').value;
     let urgency = document.getElementById('urgency').value;
 
+    // let status ='todo';
+    // let id = Math.round(Math.random() * 10000);
+
     let task = {
+        'id': id,
         'titel': titel,
         'category': category,
         'description': description,
@@ -27,4 +53,21 @@ function addTask() {
     allTasks.push(task);
     backend.setItem('allTasks', JSON.stringify(allTasks));
 
+}
+
+function showUsers() {
+    document.getElementById('assignet-to-content').innerHTML = '';
+
+    for (let i = 0; i < users.length; i++) {
+        let user = users[i];
+
+        document.getElementById('assignet-to-content').innerHTML += `
+        <div>
+        <div class="user">
+        <img class="imgcyrcle" src="img/user/${user['image']}" id="${user['id']}">
+        <span>${user['name']}</span>
+        <img class="icon-plus" src="./assets/img/icons8-plus.png"> 
+        </div>
+        </div>`;
+    }
 }
