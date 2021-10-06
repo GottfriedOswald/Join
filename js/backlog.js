fdeb39db9e2f01e24167fb5b207264ebf5f1958e
Michelle Heintze <michelle-heintze@web.de>
2021-10-06 15:50:20 +0200

    add showAllTask function

--- js/backlog.js
+++ js/backlog.js
@@ -1,3 +1,34 @@
 function init() {
-    includeHTML()
+    includeHTML();
+    loadFromBackend();
+    showAllTasks();
+
+}
+
+async function loadFromBackend() {
+    await downloadFromServer();
+    allTasks = JSON.parse(backend.getItem('allTasks')) || [];
+    console.log('Loaded from backend allTasks', allTasks);
+}
+
+function showAllTasks() {
+    for (let i = 0; i < allTasks.length; i++) {
+        document.getElementById('table').innerHTML += `
+    <tbody>
+    <tr class="table-row">
+        <td class="table-profil">
+            <div class="profil-content">
+                <img id="profile-img" class="profil-img" src="">
+                <div class="profil-name-email">
+                    <div id="profil-name" class="profil-name">${allTasks['titel']}</div>
+                    <a id="profil-email" href="#">test@guest.de</a>
+                </div>
+            </div>
+        </td>
+        <td>Managment</td>
+        <td>info</td>
+    </tr>
+</tbody>
+    `;
+    }
 }
