a793ba0043e09d02cab0bc9bde19d0570309bc48
Michelle Heintze <michelle-heintze@web.de>
2021-10-18 18:09:39 +0200

    update

--- js/navbar.js
+++ js/navbar.js
@@ -9,17 +9,6 @@ function hideMenu() {
     document.getElementById('overlay-menu').classList.add('hide-menu');
 }
 
-// function openMenu() {
-//     navbar = document.getElementById('overlay-menu');
-
-//     if (navbar.classList.contains("hide-menu")) {
-//         showMenu();
-//     } else {
-//         hideMenu();
-//     }
-// }
-
-
 function currentProfil() {
     let profilIcon = document.getElementById('profil-picture');
     profilIcon.src = currentUser[0].img;
