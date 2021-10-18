7219cb7c05e0ca4e7b3e9cb1c44863bc9ce67ea1
Michelle Heintze <michelle-heintze@web.de>
2021-10-18 18:32:00 +0200

    test

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,16 @@
 function showMenu() {
+    document.getElementById('overlay').classList.remove('d-none');
     document.getElementById('overlay-menu').classList.remove('hide-menu');
     document.getElementById('overlay-menu').classList.add('show-menu');
 
 }
 
 function hideMenu() {
+    document.getElementById('overlay').classList.remove('d-none');
     document.getElementById('overlay-menu').classList.remove('show-menu');
     document.getElementById('overlay-menu').classList.add('hide-menu');
 }
+
 
 function currentProfil() {
     let profilIcon = document.getElementById('profil-picture');
