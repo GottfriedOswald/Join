3fc035cd3c611f57912eb3ae8d0923f69c7cc36a
Michelle Heintze <michelle-heintze@web.de>
2021-10-18 13:16:36 +0200

    edit navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,24 @@
-function hideMenu() {
-    document.getElementById('menu').classList.add('show-menu');
+function showMenu() {
+    document.getElementById('overlay-menu').classList.remove('hide-menu');
+    document.getElementById('overlay-menu').classList.add('show-menu');
+
 }
+
+function hideMenu() {
+    document.getElementById('overlay-menu').classList.remove('show-menu');
+    document.getElementById('overlay-menu').classList.add('hide-menu');
+}
+
+function openMenu() {
+    navbar = document.getElementById('overlay-menu');
+
+    if (navbar.classList.contains("hide-menu")) {
+        showMenu();
+    } else {
+        hideMenu();
+    }
+}
+
 
 function currentProfil() {
     let profilIcon = document.getElementById('profil-picture');
